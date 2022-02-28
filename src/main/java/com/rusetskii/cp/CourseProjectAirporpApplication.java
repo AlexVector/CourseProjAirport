8303a518da28ed10d142dd74ae57e450501fd17a
Alex <rucetskii.alexander@gmail.com>
2022-02-28 20:18:51 +0300

    Initial commit

---
+++ src/main/java/com/rusetskii/cp/CourseProjectAirporpApplication.java
@@ -0,0 +1,11 @@
+package com.rusetskii.cp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectAirporpApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseProjectAirporpApplication.class, args);
+	}
+}
