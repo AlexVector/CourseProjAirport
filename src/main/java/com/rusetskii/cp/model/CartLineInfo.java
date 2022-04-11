6901fd0f339db3199c2b6fc6426cc68a536dea2f
Alex <rucetskii.alexander@gmail.com>
2022-04-11 15:34:01 +0300

    Ticket buying and acception. Cart page, personal data page Some new fixes

---
+++ src/main/java/com/rusetskii/cp/model/CartLineInfo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rusetskii.cp.model;
+
+public class CartLineInfo {
+  
+    private TicketInfo ticketInfo;
+    private int quantity;
+  
+    public CartLineInfo() {
+        this.quantity = 0;
+    }
+  
+    public TicketInfo getTicketInfo() {
+        return ticketInfo;
+    }
+  
+    public void setTicketInfo(TicketInfo ticketInfo) {
+        this.ticketInfo = ticketInfo;
+    }
+  
+    public int getQuantity() {
+        return quantity;
+    }
+  
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+  
+    public double getAmount() {
+        return this.ticketInfo.getPrice() * this.quantity;
+    }
+     
+}
